158b44f1730da096cb9fa485b55fa6d183437476
RUIYANG ZHANG <zhangruiyang36@gmail.com>
2020-03-15 23:30:21 +0800

    change api

--- src/clients/api.js
+++ src/clients/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosService = axios.create({
-  baseURL: 'http://3.21.10.77:3001',
+  baseURL: 'http://52.15.120.145:3001',
   // baseURL: 'http://3.21.10.77:3000',
 
   timeout: 10000,
